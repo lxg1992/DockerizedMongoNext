d554df2fab53bf356a85201d37e68a319316df61
Alexey <lxg1992@gmail.com>
2022-11-26 21:41:08 -0500

    Main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import React from 'react'
+// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { jsx, ThemeProvider } from 'theme-ui';
+
+
+import theme from '../theme';
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import React from 'react'
+
+import Link from "next/link";
+
+const Nav = () => (
+  <header
+    sx={{
+      height: "60px",
+      width: "100vw",
+      bg: "primary",
+      borderBottom: "1px solid",
+      borderColor: "primary",
+    }}
+  >
+    <nav
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        variant: "containers.page",
+        height: "100%",
+      }}
+    >
+      <Link href="/" sx={{ fontWeight: "bold", fontSize: 4, cursor: "pointer" }}>
+        Home
+      </Link>
+
+      <Link href="/notes" sx={{ color: "text", fontSize: 3, cursor: "pointer" }}>
+        Notes
+      </Link>
+    </nav>
+  </header>
+);
+
+export default Nav;
